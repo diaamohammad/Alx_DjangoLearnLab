6590dec35f6267664e6f081d6871f549c0ae7aa5
diaa mohammad <diaamohammad078@gmail.com>
2024-12-08 22:04:21 +0200

    advance api

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Author(models.model):
+class Author(models.Model):
     name = models.CharField(max_length=10)
     def __str__(self):
         return self.name
-class Book(models.model):
+class Book(models.Model):
     title = models.CharField(max_length=10)
     publication_year = models.IntegerField()
     author =  models.ForeignKey(Author,on_delete=models.CASCADE)
