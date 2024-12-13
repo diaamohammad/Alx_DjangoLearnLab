e3501689d34b8a93dccc0846abd346bff84ee5fa
diaa mohammad <diaamohammad078@gmail.com>
2024-12-13 16:20:29 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,14 +2,10 @@ from rest_framework import routers
 from .views import ListView, DetailView, CreateView, UpdateView, DeleteView , AuthorView
 from django.urls import path ,include
 
-router = routers.DefaultRouter()
-router.register(r'books', ListView, basename='books')
-router.register(r'books/<int:pk>', DetailView, basename='books_detail')
-router.register(r'books/create', CreateView, basename='create')
-router.register(r'books/update/<int:pk>', UpdateView, basename='update')
-router.register(r'books/delete/<int:pk>', DeleteView, basename='delete')
-router.register(r'users', AuthorView, basename='users')
-
 urlpatterns =[
-    path('', include(router.urls))
+    path('books/',ListView.as_view(), name='book-list'), 
+    path('books/<int:pk>/', DetailView.as_view(), name='book-detail'),  # لعرض كتاب واحد
+    path('books/create/', CreateView.as_view(), name='book-create'),  # لإنشاء كتاب جديد
+    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),  # لتحديث كتاب
+    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),  # لحذف كتاب
 ]
