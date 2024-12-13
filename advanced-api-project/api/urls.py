4d63a2ef4e58aea360d6da3b3647630cb915c92b
diaa mohammad <diaamohammad078@gmail.com>
2024-12-13 16:37:17 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,12 +2,14 @@ from rest_framework import routers
 from .views import ListView, DetailView, CreateView, UpdateView, DeleteView , AuthorView
 from django.urls import path ,include
 
+router = routers.DefaultRouter()
+router.register(r'books', ListView, basename='books')
+router.register(r'books/(?P<pk>\d+)', DetailView, basename='books_detail')  # Use pk instead of id here
+router.register(r'books/create', CreateView, basename='create')
+router.register(r'books/update/(?P<pk>\d+)', UpdateView, basename='update')
+router.register(r'books/delete/(?P<pk>\d+)', DeleteView, basename='delete')
+router.register(r'authors', AuthorView, basename='authors')  # Changed 'users' to 'authors'
+
 urlpatterns = [
-    # مسارات للكتب
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail-update-destroy'),
-    
-    # مسارات للمؤلفين
-    path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
-    path('authors/<int:pk>/', AuthorRetrieveView.as_view(), name='author-detail'),
+    path('', include(router.urls))
 ]
