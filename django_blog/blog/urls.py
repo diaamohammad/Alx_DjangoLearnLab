7f6d68ee3833a6d1b5f281da56ff98695edafdf8
diaa mohammad <diaamohammad078@gmail.com>
2024-12-14 22:52:31 +0200

    Post Management

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('profile/', profile_view, name='profile'),
     path('posts/', PostListView.as_view(), name = 'post-list'),
-    path('posts/new/', PostCreateView.as_view(), name= 'post-create'),
+    path('post/new/', PostCreateView.as_view(), name= 'post-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name= 'post-delete'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name= 'post-delete'),
         
     
 ]
