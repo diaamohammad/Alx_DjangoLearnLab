960b8c2fa3052815ed4aa732d3b479007b48402f
diaa mohammad <diaamohammad078@gmail.com>
2024-12-15 22:55:34 +0200

    Post Management

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -17,8 +17,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     author= models.ForeignKey(User,on_delete=models.CASCADE)
     content=models.CharField(max_length=100)
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now_add=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.author} :{self.content}"
