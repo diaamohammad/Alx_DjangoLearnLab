551f9db1649019a46a85cc4c2c6d4cc2cc3e1bfd
diaa mohammad <diaamohammad078@gmail.com>
2024-12-20 21:24:18 +0200

    add tag

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 from .models import Post,Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -17,7 +18,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields= ['title','content','published_date','author','tags']
-
+    
+    tags = forms.CharField(
+        widget=TagWidget(attrs={'placeholder': 'Add tags, separated by commas'}),
+        required=False  
+    )
 
 class CommentForm(forms.ModelForm):
 
