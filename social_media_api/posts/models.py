b94d7ff90580e5e3c443b276bb6d35081365999f
diaa mohammad <diaamohammad078@gmail.com>
2024-12-25 23:20:00 +0200

    post app

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    content = models.CharField(max_length=30)
-    title = models.CharField(max_length=30)
+    content = models.TextField(max_length=30)
+    title = models.TextField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post,related_name=commments,on_delete=models.CASCADE)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
-    content = models.CharField(max_length=70)
+    content = models.TextField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
