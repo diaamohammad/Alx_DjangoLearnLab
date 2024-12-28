a2d4e42b563866355d89c4d1687058d4a3021d6e
diaa mohammad <diaamohammad078@gmail.com>
2024-12-28 23:40:01 +0200

    create followers

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import CommentView,PostView
+from .views import CommentView,PostView,FeedAPIView
+from django.urls import path
+
 
 router = DefaultRouter
 
 router.register(r'posts',PostView)
 router.register(r'comments',CommentView)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('feed/',FeedAPIView.as_view(),name = 'feed')
+]
